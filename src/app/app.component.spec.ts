896bf64316f32fa635eaf4cf5aaad5f372c26428
acejarvis <acejarvis98@gmail.com>
2019-07-19 11:57:46 -0400

    added basic components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SumobotWeb'`, () => {
+  it(`should have as title 'McMaster Sumobot'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('SumobotWeb');
+    expect(app.title).toEqual('McMaster Sumobot');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SumobotWeb!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to McMaster Sumobot!');
   });
 });
